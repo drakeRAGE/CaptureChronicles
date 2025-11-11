7457b3dfb6f3b7be61228de34bfc9c7152c6b33a
ShubhamDudhbhate <shubhamdudhbhate2006@gmail.com>
2025-11-11 15:32:58 +0530

    update

---
+++ gallery/src/setupTests.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+
+// Global test setup
+global.ResizeObserver = class ResizeObserver {
+  constructor(cb) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }], this);
+  }
+  unobserve() {}
+  disconnect() {}
+};
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
